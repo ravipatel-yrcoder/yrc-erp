20f52a66b99ddbfbdc694573aef700c3e7b08836
Ravi Moghariya <ravipatel96013@gmail.com>
2026-04-14 11:47:45 +0530

    working on send email

---
+++ App/models/Company.php
@@ -0,0 +1,42 @@
+<?php
+class Models_Company extends TinyPHP_ActiveRecord
+{
+    public $tableName = "companies";
+
+    public $name = "";
+    public $email = null;
+    public $phone = null;
+    public $address = null;
+    public $city = null;
+    public $country = null;
+    public $zipcode = null;
+    public $contact_name = null;
+    public $contact_email = null;
+    public $contact_phone = null;
+    public $plan = "free";
+    public $status = "active";
+    public $timezone = "UTC";
+    public $currency = "INR";
+    public $created_at = null;
+    public $updated_at = null;
+
+    protected $dbIgnoreFields = ["id"];
+
+    public function init() {
+        $this->addListener('beforeCreate', array($this, 'doBeforeCreate'));
+        $this->addListener('beforeUpdate', array($this, 'doBeforeUpdate'));
+    }
+
+    protected function doBeforeCreate() {
+        $date = date("Y-m-d H:i:s");
+        $this->created_at = $date;
+        $this->updated_at = $date;
+        return !$this->hasErrors();
+    }
+
+    protected function doBeforeUpdate() {
+        $this->updated_at = date("Y-m-d H:i:s");
+        return !$this->hasErrors();
+    }    
+}
+?>
